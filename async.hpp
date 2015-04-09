d3c75eb8cfd6b89a4d07c45a7ca5ce3f1049c0b3
shilkin <shilkin@corp.sputnik.ru>
2015-04-09 15:43:18 +0300

    Async is like Work, but without thread pool

--- async.hpp
+++ async.hpp
@@ -2,36 +2,38 @@
 
 #include "handle.hpp"
 #include "error.hpp"
+#include "loop.hpp"
 #include <chrono>
 #include <iostream>
+#include <memory>
 
 namespace uvpp
 {
     class Async : public handle<uv_async_t>
     {
     public:
-        Async(std::function<void()> callback):
-            handle<uv_timer_t>()
+        Async(loop &l): handle<uv_async_t>(), loop_(l.get())
         {
-            init(uv_default_loop(), callback);
+            
         }
-
-        Async(loop &l, std::function<void()> callback):
-            handle<uv_timer_t>()
+        
+        Async(): handle<uv_async_t>(), loop_(uv_default_loop())
         {
-            init(l.get(), callback);
+            
         }
-
-        void init(uv_loop_t *l, std::function<void()> callback) {
+        
+        error send(std::function<void()> callback) {
             callbacks::store(get()->data, internal::uv_cid_async, callback);
 
-            uv_async_init(l, get(), [](uv_async_t* handle){
+            uv_async_init(loop_, get(), [](uv_async_t* handle){
                 callbacks::invoke<decltype(callback)>(handle->data, internal::uv_cid_async);
-            });            
+            });
+            
+            return error(uv_async_send(get()));
         }
 
-        error send() {
-            return error(uv_asenc_send(get()));
-        }
+    private:
+        uv_loop_t *loop_;   
+
     };
 }
