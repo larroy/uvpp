9a38bd2dcf786042e1db00880882fb38b1d39df5
smallwinds <578057061@qq.com>
2018-07-19 17:34:00 +0800

    update demo

--- test/main.cpp
+++ test/main.cpp
@@ -1,69 +1,16 @@
-#include <iostream>
-#include <chrono>
 #include <thread>
-#include <vector>
-#include <set>
+#include <iostream>
+#include <string>
+#include "Server.h"
 
-// uvpp
-#include "uvpp/loop.hpp"
-#include "uvpp/timer.hpp"
-#include "uvpp/async.hpp"
-#include "uvpp/work.hpp"
-#include "uvpp/tcp.hpp"
-#include "uvpp/idle.hpp"
-#include "uvpp/resolver.hpp"
-
-#include <memory>
-#include <limits>
-
-void myerror(uvpp::error e)
-{
-    if (e)
-        std::cout << "UVPP Error: " << e.str() << std::endl;
-}
+using namespace std;
 
 int main()
 {
-    uvpp::loop loop;
-    uvpp::Resolver resolver(loop);
-    resolver.resolve("localhost", [](const uvpp::error& error, bool ip4, const std::string& addr)
-    {
-        if (error)
-        {
-            std::cout << error.str() << std::endl;
-            return;
-        }
-        std::cout << (ip4 ? "IP4" : "IP6") << ": " << addr << std::endl;
-    });
+    Server s;
 
+    s.set_port(12345);
+    s.start();
 
-#if 0
-   uvpp::Tcp tcp(loop);
-   if (!tcp.connect("127.0.0.1", 33013, [](auto e){
-
-       std::cout << "connected: " << e.str() << std::endl;
-   }))
-       std::cout << "error connect\n";
-#endif
-
-   std::thread t([&loop]()
-   {
-      std::cout << "Thread started: "<< std::this_thread::get_id() << std::endl;
-      try
-      {
-        if (!loop.run())
-        {
-            std::cout << "error run\n";
-        }
-        std::cout << "Quit from event loop" << std::endl;
-      }
-      catch(...)
-      {
-          std::cout << "exception\n";
-      }
-   });
-
-   t.join();
-
-   return 0;
+    return 0;
 }
